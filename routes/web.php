844c88e80a0f30d1fd98e93b76069b5b62a46613
Gadis Syifa Anisah <anisahsyifa1407@gmail.com>
2024-09-25 08:30:41 +0700

    laravel/ui & import asset template

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes([
+    'register' => false,
+    'reset' => false,
+    'verify' => false
+    
+]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
